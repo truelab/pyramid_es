7d7f204ade7902c3f312b25e5cc5afcafb319cc5
davidemoro <davide.moro@gmail.com>
2015-04-15 14:49:05 +0200

    added the concept of fallback adapter if no IElastic component is found

--- pyramid_es/util.py
+++ pyramid_es/util.py
@@ -1,6 +1,7 @@
 from pyramid.threadlocal import get_current_registry
 
 from .interfaces import IElastic
+from .elastic import ElasticBWC
 
 
 def get_registry_from_resource(resource):
@@ -15,4 +16,11 @@ def get_registry_from_resource(resource):
 def get_elastic_adapter(resource):
     registry = get_registry_from_resource(resource)
     elastic_adapter = registry.queryAdapter(resource, IElastic)
+    if elastic_adapter is None:
+        # If no IElastic adapter is found, we assume you are
+        # working with the traditional method based on mixin
+        # class. We just return the fallback default adapter.
+        # We don't use the default keyword argument of
+        # the queryAdapter method, less overhead
+        elastic_adapter = ElasticBWC(resource)
     return elastic_adapter
